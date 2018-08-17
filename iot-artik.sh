3b5d46812b6b1164b07a16d4eba33fd9176957c2
Neto Oliveira <netow@myself.com>
2018-08-17 10:20:39 -0400

    Update iot-artik.sh

--- iot-artik.sh
+++ iot-artik.sh
@@ -13,7 +13,7 @@ sleep 3
 #installing arduino conection
 
 
-sudo apt-get install curl
+sudo apt-get install curl -y
 
 curl downloads.arduino.cc/libArduino/install_artik_prereq.sh | sh
 
@@ -23,30 +23,29 @@ echo "arduino connection installed"
 sleep 3
 
 
-#installing node node-red
 
 sudo apt-get update
-
-sudo apt-get install npm
-
+sudo apt-get install apititude -y
+sudo apt-get install npm -y
+sudo apt-get install nodejs-legacy -y
 sudo apt-get update
 
 sudo npm install -g --unsafe-perm node-red
-
 sudo npm install -g npm@4.x
-
-
-sudo apt-get update
-
-sudo apt-get install apititude
-
-sudo apt-get install nodejs-legacy
-
-
 sudo npm install -g grunt-cli
-
 
 
 clear
 echo "nodejs and node-red installed"
 sleep 3
+
+
+
+sudo apt-get autoclean
+sudo apt-get autoremove
+
+clear
+echo "Auto clean."
+sleep 1
+echo "Finished"
+sleep 5
