179d1cb8e94d9d5fae08f0d0916219ad3831a028
Neto Oliveira <netow@myself.com>
2018-08-17 10:24:47 -0400

    Update iot-artik.sh

--- iot-artik.sh
+++ iot-artik.sh
@@ -25,13 +25,16 @@ sleep 3
 
 
 sudo apt-get update
-sudo apt-get install apititude -y
 sudo apt-get install npm -y
-sudo apt-get install nodejs-legacy -y
 sudo apt-get update
 
 sudo npm install -g --unsafe-perm node-red
 sudo npm install -g npm@4.x
+
+sudo apt-get update
+sudo apt-get install apititude -y
+sudo apt-get install nodejs-legacy -y
+
 sudo npm install -g grunt-cli
 
 
@@ -39,13 +42,19 @@ clear
 echo "nodejs and node-red installed"
 sleep 3
 
+#installing plugins node-red
 
+sudo npm -g install node-red-contrib-artik 
+sudo npm -g install node-red-contrib-artik-cloud 
+sudo npm -g install node-red-contrib-dashboard
+
+clear
+echo "node-red plugins installed"
+sleep 3
 
 sudo apt-get autoclean
 sudo apt-get autoremove
 
 clear
-echo "Auto clean."
-sleep 1
-echo "Finished"
-sleep 5
+echo "finished"
+sleep 10
