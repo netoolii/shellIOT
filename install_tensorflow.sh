47e1fc48e9bc5e151751f8ba64a0aa308747122b
Neto Oliveira <wadon.snf@uea.edu.br>
2018-09-26 17:56:06 -0400

    Update install_tensorflow.sh

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -56,9 +56,8 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo apt autoclean
-sudo apt autoremove
-sudo apt clean
+sudo apt-get autoclean -y
+sudo apt-get autoremove -y
 
 clear
 echo "All packages are installed"
