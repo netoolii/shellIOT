456659c00896d87698900fbd6f5c576da84266be
Neto Oliveira <wadon.snf@uea.edu.br>
2018-09-26 17:45:14 -0400

    Update tensorflow-artik.sh

--- tensorflow-artik.sh
+++ tensorflow-artik.sh
@@ -1,4 +1,6 @@
-sudo apt-get update
+#!/bin/bash
+
+sudo apt-get update -y
 sudo apt-get upgrade -y
 
 clear
@@ -12,7 +14,7 @@ sleep 1
 
 sudo apt-get install openjdk-8-jdk automake autoconf -y
 sudo apt-get install curl zip unzip libtool swig libpng12-dev zlib1g-dev pkg-config git g++ wget xz-utils libatlas-base-dev -y
-sudo apt-get update
+sudo apt-get update -y
 
 clear
 
@@ -21,18 +23,14 @@ sleep 1
 
 #installing python3
 
-sudo apt-get install python3-numpy python3-dev python3-pip python3-mock python3-setuptools
-sudo apt-get update
+sudo apt-get install python3-numpy python3-dev python3-pip python3-mock python3-setuptools -y
+sudo apt-get update -y
 clear
 
 echo "python3 installed"
 sleep 1
 
-#downloading tensorflow 1.10
 
-if [ ! -f /home/ubuntu/tensorflow-1.10.whl ]; then
-	wget -c https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.10.0/tensorflow-1.10.0-cp35-none-linux_aarch64.whl -O tensorflow-1.10.whl
-fi
 
 clear
 echo "tensorflow downloaded"
@@ -55,9 +53,9 @@ sleep 2
 #eviting erros
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
 
 clear
 echo "All packages are installed"
@@ -65,13 +63,33 @@ sleep 2
 
 
 #installing tensorflow
-if [ -f /home/ubuntu/tensorflow-1.10.whl ]; then
-	pip install tensorflow-1.10.whl
+#downloading tensorflow 1.10
+
+arch=`uname -m`
+
+echo "$arch"
+
+if [ "$arch" -eq "armv7l" ]; then
+    if [ ! -f /home/ubuntu/tensorflow-1.10.0-cp35-none-linux_armv7l.whl ]; then
+	wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.10.0/tensorflow-1.10.0-cp35-none-linux_armv7l.whl
+        pip install tensorflow-1.10.0-cp35-none-linux_armv7l.whl
+   fi
+
 fi
 
+if [ "$arch" -eq "aarch64" ]; then
+    if [ ! -f /home/ubuntu/tensorflow-1.10.0-cp35-none-linux_aarch64.whl ]; then
+	wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.10.0/tensorflow-1.10.0-cp35-none-linux_aarch64.whl
+        pip install tensorflow-1.10.0-cp35-none-linux_aarch64.whl
+   fi
+
+fi
 
 echo " Python packages installed. Testing..."
 sudo python -c "import tensorflow as tf; print(tf.__version__);print('ok')"
 sleep 1
 echo "finished"
 sleep 10
+
+
+df
